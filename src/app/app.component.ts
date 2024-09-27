d4e12ccbc657cb10df845d84fabdb3f3f42ba610
Rostja <rasto.dorcak@gmail.com>
2024-09-27 15:21:03 +0200

    #	modified:   src/app/app.component.html #	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { AboutComponent } from './components/about/about.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, FooterComponent,
+     ClientsComponent, AboutComponent, PricingComponent, TestimonialsComponent,
+      GalleryComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
