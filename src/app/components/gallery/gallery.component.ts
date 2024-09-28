ff5d46e021d087a234209a16c2d781077a660eec
Rostja <rasto.dorcak@gmail.com>
2024-09-28 22:05:30 +0200

    # Changes to be committed: #	modified:   src/app/components/clients/clients.component.html #	modified:   src/app/components/clients/clients.component.ts #	modified:   src/app/components/gallery/gallery.component.ts #	modified:   src/app/components/pricing/pricing.component.html #	modified:   src/app/components/pricing/pricing.component.ts

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+Gallery = { 
+  title: "GALLERY",
+  description: "Project images",
+  detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam! ",
+  GalleryImages: [
+    {id: 1, url: "../assets/images/gallery-images/gallery-image-1.jpg", caption: "Tree" },
+    {id: 2, url: "../assets/images/gallery-images/gallery-image-2.jpg", caption: "Fingerprint" },
+    {id: 3, url: "../assets/images/gallery-images/gallery-image-3.jpg", caption: "The man" },
+    {id: 4, url: "../assets/images/gallery-images/gallery-image-4.jpg", caption: "Mustache" },
+    {id: 5, url: "../assets/images/gallery-images/gallery-image-5.jpg", caption: "Moose" },
+    {id: 6, url: "../assets/images/gallery-images/gallery-image-6.jpg", caption: "Justice" }
 
+  ]
+ }
 }
